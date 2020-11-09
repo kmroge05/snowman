26fc9ca61ccdea99a0ed93425590ac0fb1eba156
Kory Rogers <kmroge05@gmail.com>
2020-11-09 00:35:41 -0500

    Added comments to layout game play

--- Snowman/GamePlay.cs
+++ Snowman/GamePlay.cs
@@ -6,5 +6,21 @@ namespace Snowman
 {
     class GamePlay
     {
+
+        // Welcome player and ask for their name
+        // Greet player
+        // Explain the game
+        
+        // Randomly select phrase from CSV and Titles Class
+        
+        // Print Category and blanked-out title
+        
+        // Ask for guess
+        // If guess is right, display title with letters filled in
+        // If guess is wrong, displays portion of Snowman, and adds to number of wrong guesses
+        // Repeats until 9 wrong guesses or puzzle is solved
+        // Congratulate player is puzzle is solved OR Apolgize if player fails
+
+        // Allow player to exit game by typing quit at any time.
     }
 }
