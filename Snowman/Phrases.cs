f819314f5701f0c1c7fd49ca144a6b5161ad0c5a
Kory Rogers <kmroge05@gmail.com>
2020-11-09 00:15:06 -0500

    Add project files.

---
+++ Snowman/Phrases.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snowman
+{
+    class Phrases
+    {
+        public static string Phrase { get; set; }
+        public static string Category { get; set; }
+    }
+}
